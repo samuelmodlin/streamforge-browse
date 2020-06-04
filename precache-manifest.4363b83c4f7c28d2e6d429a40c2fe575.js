9a94143dccc57fec73a770954f08e7f8d868ee63
unknown <modlin.samuel@gmail.com>
2020-06-04 05:46:46 -0700

    deploy

---
+++ precache-manifest.4363b83c4f7c28d2e6d429a40c2fe575.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e3bddf1f500b1e6429",
+    "url": "/streamforge-browse/css/app.5018fe0c.css"
+  },
+  {
+    "revision": "e5e0c6756102be60b5cd",
+    "url": "/streamforge-browse/css/chunk-vendors.479c06d3.css"
+  },
+  {
+    "revision": "15d1b51fe672c73b327478cbba87776a",
+    "url": "/streamforge-browse/img/streamforge-logo.15d1b51f.svg"
+  },
+  {
+    "revision": "4cf36e620f51976c669c6244648f9f32",
+    "url": "/streamforge-browse/index.html"
+  },
+  {
+    "revision": "60e3bddf1f500b1e6429",
+    "url": "/streamforge-browse/js/app.2fe44555.js"
+  },
+  {
+    "revision": "e5e0c6756102be60b5cd",
+    "url": "/streamforge-browse/js/chunk-vendors.a0ac070f.js"
+  },
+  {
+    "revision": "c15596da3ed2bce51e102040175194ca",
+    "url": "/streamforge-browse/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/streamforge-browse/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/streamforge-browse/precache-manifest.4363b83c4f7c28d2e6d429a40c2fe575.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "streamforge-browse"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});

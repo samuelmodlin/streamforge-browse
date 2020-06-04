b9819118affe8cc4da44d255a1120185467b1c44
unknown <modlin.samuel@gmail.com>
2020-06-04 05:39:35 -0700

    Update

---
+++ precache-manifest.ebffaaba740fcc210d90a6b08d3dfbc7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40b335dcf67c69bec4c492efac9d5952",
+    "url": "/streamforge-dashboard/404.html"
+  },
+  {
+    "revision": "92a36f8b03d649bebca7",
+    "url": "/streamforge-dashboard/css/app.5018fe0c.css"
+  },
+  {
+    "revision": "e5e0c6756102be60b5cd",
+    "url": "/streamforge-dashboard/css/chunk-vendors.479c06d3.css"
+  },
+  {
+    "revision": "15d1b51fe672c73b327478cbba87776a",
+    "url": "/streamforge-dashboard/img/streamforge-logo.15d1b51f.svg"
+  },
+  {
+    "revision": "4fffb887e9e7a2e0acc3737d5bb38ebd",
+    "url": "/streamforge-dashboard/index.html"
+  },
+  {
+    "revision": "92a36f8b03d649bebca7",
+    "url": "/streamforge-dashboard/js/app.fbc1e421.js"
+  },
+  {
+    "revision": "e5e0c6756102be60b5cd",
+    "url": "/streamforge-dashboard/js/chunk-vendors.a0ac070f.js"
+  },
+  {
+    "revision": "c15596da3ed2bce51e102040175194ca",
+    "url": "/streamforge-dashboard/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/streamforge-dashboard/robots.txt"
+  }
+]);
